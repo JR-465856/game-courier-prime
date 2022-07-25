90dbc6d36611f503f8bd05379251c281d345d09a
JR-465856 <97478088+JR-465856@users.noreply.github.com>
2022-07-25 11:59:29 -0700

    Update RunHandler to a working and usable state

--- RunHandler.ts
+++ RunHandler.ts
@@ -1,11 +1,16 @@
 // RunHandler class
 // Used for looping and game runtime handling and such
 abstract class RunHandler {
-    // Arrays that hold the functions for updates 
-    private static onUpdateFunctions  : Array<(deltaTime: number) => void>;
-    private static onShadeFunctions   : Array<(deltaTime: number) => void>;
-    private static onPaintFunctions   : Array<(deltaTime: number) => void>;
-    private static onGameOverFunctions: Array<(deltaTime: number, isWin: boolean) => void>;
+    // Arrays that hold the functions for updates
+    //
+    // These are made public so that the RunHandler
+    // namespace can change these
+    // DO NOT CHANGE THESE MANUALLY UNLESS YOU
+    // KNOW WHAT YOU ARE DOING!!!
+    public static onUpdateFunctions  : Array<(deltaTime: number) => void>;
+    public static onShadeFunctions   : Array<(deltaTime: number) => void>;
+    public static onPaintFunctions   : Array<(deltaTime: number) => void>;
+    public static onGameOverFunctions: Array<(deltaTime: number, isWin: boolean) => void>;
     // Variables to calculate the delta time, we have these
     // variables because we want stuff not to lag (e.g. a 2 second
     // wait actually taking 2.5 seconds before "completion")
@@ -14,7 +19,13 @@ abstract class RunHandler {
     private static oldPaintRuntime   : number;
     private static oldGameOverRuntime: number;
 
-    constructor() {
+    constructor() {}
+
+    // Main function of RunHandler that handles the internals
+    //
+    // Do not call main, it is called once in RunHandler.ts and
+    // should not be called more than once
+    public static main() {
         // Initialize the arrays. TypeScript has some weird
         // bug where improperly-initialized class arrays
         // freak out and fail to work at runtime
@@ -69,14 +80,36 @@ abstract class RunHandler {
             RunHandler.oldGameOverRuntime = currentRuntime;
         });
     }
-
-    
-
 }
 
 
 
 // RunHandler namespace
 namespace RunHandler {
-
+    // onUpdate
+    export namespace onUpdate {
+        export function bind(callbackFunction: (deltaTime: number) => void) {
+            RunHandler.onUpdateFunctions.push(callbackFunction)
+        }
+    }
+    // onShade
+    export namespace onShade {
+        export function bind(callbackFunction: (deltaTime: number) => void) {
+            RunHandler.onShadeFunctions.push(callbackFunction)
+        }
+    }
+    // onPaint
+    export namespace onPaint {
+        export function bind(callbackFunction: (deltaTime: number) => void) {
+            RunHandler.onPaintFunctions.push(callbackFunction)
+        }
+    }
+    // onGameOver
+    export namespace onGameOver {
+        export function bind(callbackFunction: (deltaTime: number, isWin: boolean) => void) {
+            RunHandler.onGameOverFunctions.push(callbackFunction)
+        }
+    }
 }
+
+RunHandler.main()
